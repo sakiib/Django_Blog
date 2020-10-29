f352891b1d40592b86abc402e476a37be3fd364a
sakibalamin <sakibalamin162@gmail.com>
2020-10-29 12:50:42 +0600

    added base.html to use template inheritance & some dummy data to render

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'sakib',
+        'title': 'blog post 1',
+        'content': 'first post content'
+    },
+    {
+        'author': 'alamin',
+        'title': 'blog post 2',
+        'content': 'second post content'
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
